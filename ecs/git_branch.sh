f997f2b9420aa4b7e18efb68fc63205efa458241
moon91 <moon91@qcells.com>
2024-11-21 10:12:31 +0900

    [Add] Add git command

---
+++ ecs/git_branch.sh
@@ -0,0 +1,10 @@
+#git command
+git checkout -b [branch_name]
+
+gh repo clone [repo_name] -- --branch [branch_name]
+
+git branch
+
+git add .
+git commit -m"Commit message"
+git push origin [branch_name]
